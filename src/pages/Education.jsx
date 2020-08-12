890cbbabbfedebf188a81bdca5ba203030a22752
Aneesha Sampath <aneesha.sampath@outlook.com>
2020-08-11 20:56:24 -0400

    Add coursework

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -13,6 +13,16 @@ function Education () {
                     <li>Bachelor of Science in Artificial Intelligence, <i>School of Computer Science</i></li>
                     <li>Minor in Drama, <i>School of Drama</i></li>
                 </ul>
+
+                <p>Relevant Coursework:</p>
+                <ul>
+                    <li>Natural Language Processing</li>
+                    <li>Parallel and Sequential Algorithms and Data Structures</li>
+                    <li>Artificial Intelligence</li>
+                    <li>Great Ideas in Theoretical Computer Science</li>
+                    <li>Principles of Functional Programming</li>
+                    <li>Matrices and Linear Transformations</li>
+                </ul>
             </ul>
             <br/>
         </div>
